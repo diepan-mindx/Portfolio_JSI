ae68ad2491ffeaa066f363d73c17a10795899909
diepan-mindx <diepau1312@mindx.net.vn>
2026-01-14 20:00:08 +0700

    Update firebase-cdn.js

--- firebase-cdn.js
+++ firebase-cdn.js
@@ -5,16 +5,11 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlU-OSNz1t4oV98OzZEOPKoDtSKsTzeDs",
-  authDomain: "jsi40-19cf0.firebaseapp.com",
-  projectId: "jsi40-19cf0",
-  storageBucket: "jsi40-19cf0.firebasestorage.app",
-  messagingSenderId: "340832558663",
-  appId: "1:340832558663:web:efb3ebff120725c166db62",
-  measurementId: "G-C0ZJG8CJ89",
+  // TODO: config code
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
+
