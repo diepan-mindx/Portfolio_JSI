1724436a24abe607e4ca511320a5248df0031334
diepan-mindx <diepau1312@mindx.net.vn>
2026-01-14 20:08:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function login() {
 
 /* ================= AUTH GUARD ================= */
 auth.onAuthStateChanged((user) => {
-  if (!user && !location.pathname.includes("index") && !location.pathname.includes("signin")) {
+  if (!user && (!location.pathname.includes("index") || !location.pathname.includes("signin"))) {
     window.location.href = "index.html";
   }
 });
@@ -82,3 +82,4 @@ document
 if (location.pathname.includes("home")) {
   loadProfile();
 }
+
